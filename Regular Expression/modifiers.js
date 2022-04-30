41190b3177bb92757d830d2183d887d9753de851
MM-Morsi <69103168+MM-Morsi@users.noreply.github.com>
2022-04-30 16:28:57 +0200

    reg exp literal notation vs constructor notation

--- Regular Expression/modifiers.js
+++ Regular Expression/modifiers.js
@@ -1,5 +1,6 @@
 let myString = "Hello World Web School. I love the world";
 
+// PATTERNS WITH THE LITERAL NOTATION
 let regex = /World/;
 
 console.log(myString.match(regex));
@@ -22,4 +23,6 @@ console.log("i modifier:", myString.match(regex));
 regex = /World/ig;
 console.log("ig modifiers:", myString.match(regex));
 
-// 
+// CREATE PATTERNS WITH REGEXP OBJECT CONSTRUCTOR FUNCTION
+let re = /ab+c/i; // this is literal notation 
+re = new RegExp("ab+c", "i"); // this is the constructor notation
